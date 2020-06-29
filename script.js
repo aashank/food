58c8f1c8a55cc172cfe82830aafacfbdee6e9bc6
Akshay Shankariya <65609481+aashank@users.noreply.github.com>
2020-06-29 11:28:57 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// function handleScrollAction(id) {
+//     let element = document.getElementById(id)
+//     if (element) {
+//         element.scrollIntoView({
+//             behavior: 'smooth'
+//         })
+//     }
+
+// }
+
+function handleReloadAction(id) {
+    if (id === 'home')
+        window.location.reload();
+}
